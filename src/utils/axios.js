4423635d78507999bf5088460573ae9d996b5434
Gouravshandilya69 <i.gouravshandilya@gmail.com>
2023-11-10 12:21:21 +0530

    ssl solved

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,9 @@ import axios from "axios"
 
 const instance = axios.create({
     baseURL:"http://13.200.56.10:5000/python",
-   
+    httpsAgent: {
+        rejectUnauthorized: false,
+      }
     
 })
 
